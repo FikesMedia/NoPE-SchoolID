5b0cab98cc55c178fa9e06b750e2b88f8d060280
Christopher Fikes <cfikes@fikesmedia.com>
2021-11-05 09:12:22 -0500

    Update CleverBadgeProcessing.ps1

--- clitools/CleverBadgeProcessing.ps1
+++ clitools/CleverBadgeProcessing.ps1
@@ -12,15 +12,15 @@ Expand-Archive .\badges.zip
 $BadgeLocation = (Get-Item $ThisLocation).FullName + "\badges" 
 
 #Loop through Folders
-$AllFolders = Get-ChildItem -Recurse $BadgeLocation | Where { $_.PSIsContainer }
+$AllFolders = Get-ChildItem -Recurse $BadgeLocation | Where-Object { $_.PSIsContainer }
 forEach($Folder in $AllFolders) {
     #Loop Through Images
-    $Items = Get-ChildItem -Path $Folder.FullName -Include *.png -Depth 1 | Where { ! $_.PSIsContainer }
+    $Items = Get-ChildItem -Path $Folder.FullName -Include *.png -Depth 1 | Where-Object { ! $_.PSIsContainer }
     forEach($Item in $Items){
         # 6 Digit Number 
         $NewName = $Item.Name.Substring(($Item.Name.Length) - 10, 10)
         #Destination Full Path
-        $Destination = (Get-Item $ThisLocation).FullName + "\clever\" + $NewName
+        $Destination = (Get-Item $ThisLocation).FullName + "\..\www\assets\clever\" + $NewName
         #Move Image
         Move-Item -Path $Item.FullName -Destination $Destination -Force
     }
