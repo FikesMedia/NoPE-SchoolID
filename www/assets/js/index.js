eba939340097b6d69ff3a0524a2ab1412bd06162
Christopher Fikes <cfikes@fikesmedia.com>
2022-03-25 15:26:31 -0500

    new menu

---
+++ www/assets/js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+	// Extend Session
+	var keepSessionValid = window.setInterval(
+		function(){
+			$.getJSON( "/api/validate", function( output ) {
+				// Remain Valid
+				console.log("Session Extended");
+			});
+		}, 30000
+	);
+
+	// Check Session State
+	$.getJSON( "/api/validate", function( output ) {
+		if(output.SessionState != "Valid") {
+			window.location.replace('/login.html');
+		}
+	});
+
+});
